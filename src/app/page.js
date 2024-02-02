aa1fb060994232ab01bc4864b4f464475192f9e4
김현정(Hyunjung Kim) <withcusco@gmail.com>
2024-02-02 15:08:18 +0900

    [feat] 글 읽기, 수정 기능 완성

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ export default function Home() {
     <>
       <h2>Welcome</h2>
       Hello, WEB!
+      <br></br>
+      <img width={150} src="/karina.jpeg" alt="이미지" />
     </>
   );
 }
--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,10 @@
-export default function Read(props) {
+export default async function Read(props) {
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topics = await res.json();
   return (
     <>
-      <h2>Read</h2>
-      parameters: {props.params.id}
+      <h2>{topics.title}</h2>
+      {topics.body}
     </>
   );
 }
